99936dda0a814a7a2c959b3a2962efe03a9a8761
angelopez10 <angelopez10898@gmail.com>
2020-02-29 09:43:18 -0300

    Fixed repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import Home from './js/views/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
