1bd7d2c8214af1dec5236b8226a38ce6136d016b
jarb29 <jarb29@gmail.com>
2020-03-01 01:31:30 -0300

    no message

--- src/js/views/HomeMap.js
+++ src/js/views/HomeMap.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SimpleMap from '../Components/HomeMapa/Mapa';
+
+import SideNavLeft from '../Components/HomeMapa/SideNavLeft';
 import SideNav from '../Components/HomeMapa/SideNav';
+
 
 
 
 function HomeMap() {
   return (
     <div className="App">
-      <SimpleMap />
       <SideNav />
+      <SideNavLeft />
+       <SimpleMap />
     </div>
   );
 }
