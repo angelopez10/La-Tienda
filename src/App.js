8b74cee94485160f628bab7847022ba01455607d
jarb29 <jarb29@gmail.com>
2020-03-04 15:52:12 -0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import HomeMap from './js/views/HomeMap'
+import Modall from './js/views/ModalView';
+
 
 
 
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <HomeMap />
+      <Modall />
     </div>
   );
 }
---
+++ src/js/views/ModalView.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyVerticallyCenteredModal from '../Components/Modals/Modal'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/Button'
+
+
+
+
+
+
+
+function Modall() {
+	const [modalShow, setModalShow] = React.useState(false);
+  
+	return (
+	  <ButtonToolbar>
+		<Button variant="primary" onClick={() => setModalShow(true)}>
+		  Launch vertically centered modal
+		</Button>
+  
+		<MyVerticallyCenteredModal
+		  show={modalShow}
+		  onHide={() => setModalShow(false)}
+		/>
+	  </ButtonToolbar>
+	);
+  }
+  
+
+export default Modall;
