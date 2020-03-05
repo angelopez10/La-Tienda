a2cc671346beec057fc156073702cc2b317fb6ca
angelopez10 <angelopez10898@gmail.com>
2020-03-04 23:21:16 -0300

    Modal Login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ModalView from './js/views/ModalView';
 
-
+import SignIn from './js/Components/Modals/SigIn'
 import HomeMap from './js/views/HomeMap'
 import Home from './js/views/Home';
 import Tienda from './js/views/Tienda';
@@ -18,12 +18,7 @@ function App() {
     <div className="App">
 
       <ModalView />
-
-      
-
-      <Home/>
-
-      <Tienda />
+      <SignIn />
 
 
     </div>
--- src/js/views/ModalView.js
+++ src/js/views/ModalView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyVerticallyCenteredModal from '../Components/Modals/Modal'
+import SignIn from '../Components/Modals/Modal'
 import Button from 'react-bootstrap/Button'
 import ButtonToolbar from 'react-bootstrap/Button'
 
@@ -19,7 +19,7 @@ function ModalView() {
 		  Launch vertically centered modal
 		</Button>
   
-		<MyVerticallyCenteredModal
+		<SignIn
 		  show={modalShow}
 		  onHide={() => setModalShow(false)}
 		/>
