6476bfb4f19bfcc1b44d44e006388f1e17460659
angelopez10 <angelopez10898@gmail.com>
2020-03-07 14:24:14 -0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-=======
 import Sphere from './js/Components/Sphere/Sphere'
-
-
->>>>>>> 35322f192c52ee82dfbe352dbc9ebf33b2a66802
 import ModalView from './js/views/ModalView';
 import SignIn from './js/Components/Modals/SigIn'
 import HomeMap from './js/views/HomeMap'
