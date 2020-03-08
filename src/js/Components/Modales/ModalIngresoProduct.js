b10ae30c13782c82c82f10aa3fd139cad2199481
jarb29 <jarb29@gmail.com>
2020-03-08 18:53:09 -0300

    no message

---
+++ src/js/Components/Modales/ModalIngresoProduct.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Modal from 'react-bootstrap/Modal'
+import RegistroFormProduct from "../RegistrationForm/RegistroFormProduct";
+
+
+
+
+
+export default function ModalIngresoTienda(props) {
+	return (
+	  <Modal
+		{...props}
+		size="md"
+		aria-labelledby="contained-modal-title-vcenter"
+		centered
+	  >
+		<Modal.Header closeButton>
+		</Modal.Header>
+		<Modal.Body>
+            <p>
+			  <RegistroFormProduct />
+		    </p>
+		</Modal.Body>
+	  </Modal>
+	);
+  }
+  
