089e79f195cfe5877fff528f3b5cb4b3b94ff8d4
angelopez10 <angelopez10898@gmail.com>
2020-03-18 18:35:25 -0300

    'CSSTienda'

--- src/js/Components/Tienda/Producto.js
+++ src/js/Components/Tienda/Producto.js
@@ -21,16 +21,16 @@ export default function Producto() {
         <div class="col">
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap"/>
-                        <div class="card-body">
+                    <div class="card card-style mb-2">
+                        <img class="card-img-top" src="https://picsum.photos/600/400" alt="Card image cap"/>
+                        <div class="card-body card-bg">
                             <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <div class="row">
-                                <div class="col">
+                            <div class="col">
+                                <div class="row">
                                     <p class="btn btn-danger btn-block">99.00 $</p>
                                 </div>
-                                <div class="col">
+                                <div class="row">
                                     <a href="#" class="btn btn-success btn-block">Add to cart</a>
                                 </div>
                             </div>
