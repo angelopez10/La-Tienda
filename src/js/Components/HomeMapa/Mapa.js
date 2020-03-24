82a5352b7510725eb31e0e5880d8eeca4875a2ef
jarb29 <jarb29@gmail.com>
2020-03-24 14:49:42 -0300

    map center

--- src/js/Components/HomeMapa/Mapa.js
+++ src/js/Components/HomeMapa/Mapa.js
@@ -3,21 +3,35 @@ import useSwr from "swr";
 import GoogleMapReact from "google-map-react";
 import "./Mapa.css";
 
+
 const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 const Marker = ({ children }) => children;
 
 export default function Mapa() {
-  const mapRef = useRef();
+  const mapRef = useRef(null);
   const [bounds, setBounds] = useState(null);
+  const [zoom, setZoom] = useState(10);
 
 
   const url =
     "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
   const { data, error } = useSwr(url, { fetcher });
   const tiendas = data && !error ? data.slice(0, 200) : [];
-  
+  const points = tiendas.map(tienda => ({
+    type: "Feature",
+    properties: { cluster: false, crimeId: tienda.id, category: tienda.category },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(tienda.location.longitude),
+        parseFloat(tienda.location.latitude)
+      ]
+    }
+  }));
  
+
+
 
  
 
@@ -27,15 +41,41 @@ export default function Mapa() {
         bootstrapURLKeys={{ key: 'AIzaSyDrJx4thHq6csImpMoRlB8qy00-GQuhIQw' }}
         defaultCenter={{ lat: 52.6372, lng: -1.135171 }}
         defaultZoom={15}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map }) => {
+          mapRef.current = map;
+        }}
+        onChange={({ zoom, bounds }) => {
+          setZoom(zoom);
+          setBounds([
+            bounds.nw.lng,
+            bounds.se.lat,
+            bounds.se.lng,
+            bounds.nw.lat
+          ]);
+        }}
       >
         {tiendas.map(tienda => (
+          
+
+
               <Marker
                 key={tienda.id}
                 lat={tienda.location.latitude}
                 lng={tienda.location.longitude}
                 >
-              <button className="crime-marker">
+              <button className="crime-marker"
+              onClick={() => {
+                const expansionZoom = 17;
+                mapRef.current.setZoom(expansionZoom);
+                console.log(mapRef.current);
+                mapRef.current.panTo({ lat: parseFloat(tienda.location.latitude), lng: parseFloat(tienda.location.longitude) });
+              }}>
+
                 <img src="/favicon.ico" alt="crime" />
+
+        
+
               </button>
             </Marker>
           ))}
