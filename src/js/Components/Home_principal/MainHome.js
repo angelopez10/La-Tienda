201cc1307483c009782e57c1b909904768685e8d
jarb29 <jarb29@gmail.com>
2020-04-05 19:31:45 -0400

    final

--- src/js/Components/Home_principal/MainHome.js
+++ src/js/Components/Home_principal/MainHome.js
@@ -2,9 +2,51 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/Button'
+import ModalLogin from "../Modales/ModalLogin";
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#ff8d1e',
+    border: 0,
+    borderRadius: 50,
+    ['@media (max-width:600px)']: { // eslint-disable-line no-useless-computed-key
+      width: '80px',
+      padding: '0'
+    },
+    ['@media (max-width:1000px)']: { // eslint-disable-line no-useless-computed-key
+
+    },
+    color: '#f5f3f3',
+    height: 40,
+    padding: '0 30px',
+    '&:hover': {
+      backgroundColor: '#ff8d1e',
+      borderColor: 'none',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#ff8d1e',
+      borderColor: 'none',
+    },
+    '&:focus': {
+      backgroundColor: '#ff8d1e',
+      boxShadow: 'none'
+    },
+  },
+
+
+});
 
 export default function MainHome() {
   //Checkear imagen de fondo, usar carpeta img en public
+  const [modalShow, setModalShow] = React.useState(false);
+  const classes = useStyles();
   return (
     <div className="main">
       <div className="container h-100">
@@ -16,9 +58,15 @@ export default function MainHome() {
               name=""
               placeholder="Ingresa tu dirección"
             />
-            <Link to='/login' className="search_icon">
-            <FontAwesomeIcon icon={faSearch} />
-            </Link>
+            <ButtonToolbar className={classes.root} xs={12}>
+              <Button className={classes.root} color='default' onClick={() => setModalShow(true)} xs={12}>
+                <FontAwesomeIcon icon={faSearch} />
+              </Button>
+              <ModalLogin
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
+            </ButtonToolbar>
           </div>
         </div>
       </div>
