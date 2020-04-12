3c6fde09406c0d6da03adcea0bf213899857ad97
jarb29 <jarb29@gmail.com>
2020-04-11 21:09:19 -0400

    return

--- src/js/flux.js
+++ src/js/flux.js
@@ -274,7 +274,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 				}
 				newCarrito.map(item => {
-					newtotalCarrito = newtotalCarrito + (item.cantidad * item.producto.precio);
+					return newtotalCarrito = newtotalCarrito + (item.cantidad * item.producto.precio);
 				})
 				setStore({
 					carrito: newCarrito,
@@ -308,7 +308,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					newCarrito.splice(pos, 1);
 				}
 				newCarrito.map((item) => {
-					newtotalCarrito = newtotalCarrito + (item.cantidad * item.producto.precio);
+					return newtotalCarrito = newtotalCarrito + (item.cantidad * item.producto.precio);
 				})
 				setStore({
 					carrito: newCarrito,
@@ -645,10 +645,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				store.carrito.map(ItemCarrito => {
 					store.tiendaSeleccionada.map(ItemTienda => {
 						if (ItemTienda.nombre === ItemCarrito.producto.nombre) {
-						console.log(ItemTienda.id, "id producto");
-						console.log(ItemCarrito.cantidad, "cantida prod");
-						console.log(ItemTienda.precio, "precio");
-						console.log(ItemTienda, "buscando el email");
+						
 
 							store.ItemProductoCompradoId.push(ItemTienda.id);
 							store.CantidaProductoComprado.push(ItemCarrito.cantidad);
