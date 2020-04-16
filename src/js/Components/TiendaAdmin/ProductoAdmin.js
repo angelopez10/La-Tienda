c252710e1f41f27404abbb64cb3d356ef389588c
jarb29 <jarb29@gmail.com>
2020-04-16 19:41:25 -0400

    modal

--- src/js/Components/TiendaAdmin/ProductoAdmin.js
+++ src/js/Components/TiendaAdmin/ProductoAdmin.js
@@ -7,7 +7,10 @@ import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import '../Tienda/producto.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button from 'react-bootstrap/Button'
 import { withRouter } from 'react-router-dom';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const useStyles = makeStyles(() => ({
     textColor: {
@@ -23,7 +26,7 @@ function ProductoAdmin(props) {
     return (
         <div className="col-12 col-md-6 col-lg-3 ">
             <div className="card card-style mb-2">
-                <img className="card-img-top" src={img} alt="" width='400px' height='200px'/> 
+                <img className="card-img-top" src={img} alt="" width='400px' height='200px' />
                 <div className="card-body card-bg">
                     <h4 className="card-title">{props.producto.nombre}</h4>
                     <p className="card-text">{props.producto.descripcion}</p>
@@ -32,17 +35,17 @@ function ProductoAdmin(props) {
                     <div className="col">
                         <div className="row float-right">
                             <ButtonToolbar>
-                            <IconButton aria-label="edit" className={classes.textColor} onClick={() => setModalShow(true)}>
-                                <EditIcon />
-                                <ModalEditarProducto
-                                    show={modalShow}
-                                    onHide={() => setModalShow(false)}
-                                    producto={props.producto}
-                                />
-                            </IconButton>
+                                <IconButton >
+                                        <EditIcon className={classes.textColor} onClick={() => setModalShow(true)} color='default' xs={12}/>
+                                    <ModalEditarProducto
+                                        show={modalShow}
+                                        onHide={() => setModalShow(false)}
+                                        producto={props.producto}
+                                    />
+                                </IconButton>
                             </ButtonToolbar>
                             <IconButton aria-label="delete" id={props.producto.id} className={classes.textColor} >
-                                <DeleteIcon  onClick={e => actions.deleteProducto(e, props.producto.id)}/>
+                                <DeleteIcon onClick={e => actions.deleteProducto(e, props.producto.id)} />
                             </IconButton>
                         </div>
                     </div>
